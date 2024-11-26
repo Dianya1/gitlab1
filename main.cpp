6d47447f7cae2e85c699e6e23f48e293450551e2
Dianya1 <dianabashegurova@gmail.com>
2024-11-26 20:50:55 +0500

    Созданы строки

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 
 using namespace std;
 
 void f1(){
+    string m,n;
+    vector <string> array(3);
+    ifstream in ("lala.txt");
+    if (in.is_open()){
+            in >> m >> n;
+            array[0] = m;
+            array[1] = n;
     }
+}
 void f2(){
     }
 void f3(){
